a19f200b74e713a7daeb79ab74acf06eb4da56a7
Simsaladoo <simsaladoo@hotmail.com>
2025-03-27 14:38:07 -0700

    ai code updates

--- MetadataEditor/Content/Python/editor_menus/metadata_editor.py
+++ MetadataEditor/Content/Python/editor_menus/metadata_editor.py
@@ -78,13 +78,42 @@ class MetadataEditorWidget(QtWidgets.QWidget):
         self.ui.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowMinimizeButtonHint | QtCore.Qt.WindowMaximizeButtonHint)
         ## self.ui.setWindowIcon(QtGui.QIcon(f"stroked.png"))
         layout.setMargin(0)
-        layout.addWidget(self.ui)
+        layout.addWidget(self.ui)        
+        self.init_ui()
+        
+        
+    def init_ui(self):
+        # self.ui.verticalLayout = self.ui.findChild(QtWidgets.QPushButton, 'verticalLayout')
         self.ui.pushButton_Save = self.ui.findChild(QtWidgets.QPushButton, 'pushButton_Save')
         self.ui.pushButton_Save.clicked.connect(save_asset)
+        self.scroll_area = QtWidgets.QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.inputs = {}
         
-        ## Here we need to get all the selecteds
-        selected = get_selected_assets()
-        # If we are going to allow for multi-edit -- we'll need to expand the window
+
+        # for key, value in self.data_dict.items():
+        
+        self.add_entry("Test", "Another Value")
 
         
+         ## Here we need to get all the selecteds
+        selected = get_selected_assets()
         existing_tags = get_asset_metadata(get_selected_assets()[0])
+        
+
+        # Box to add into:
+        # verticalLayout
+        
+    def add_entry(self, key, value):
+        key_label = QtWidgets.QLabel(key)
+        value_input = QtWidgets.QLineEdit(value)
+        self.inputs[key] = value_input
+        
+    def add_empty_entry(self):
+        new_key = f"Key {len(self.inputs) + 1}"
+        self.add_entry(new_key, "")
+        self.layout.adjustSize()
+        self.scroll_area.ensureWidgetVisible(self.inputs[new_key])
+    
+    def get_data(self):
+        return {key: input_field.text() for key, input_field in self.inputs.items()}
